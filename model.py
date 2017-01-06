ceabf68d07ba08f0c6a22cb68682c374e0ecb65b
David A. Ventimiglia <dventimi@gmail.com>
2017-01-06 13:01:03 -0800

    Got the pipeline to run using a generator
    The loss is terrible, however so far I haven't taken any steps to get
    my model to produce numerical outputs that are scaled like the target
    steering angles in [-1, 1].  That's the next step.

--- model.py
+++ model.py
@@ -5,74 +5,65 @@ from keras.models import Sequential
 from keras.utils import np_utils
 from keras.utils.visualize_util import plot
 from sklearn.model_selection import train_test_split
+import keras.preprocessing.image as img
 import math
 import numpy as np
+import pdb
 import pickle
-
-# Load
-
-with open('train.p', 'rb') as f:
-    data = pickle.load(f)
-with open('./test.p', mode='rb') as f:
-    test = pickle.load(f)
-
-# Split
-
-X_train, X_val, y_train, y_val = train_test_split(data['features'], data['labels'], random_state=0, test_size=0.33)
-
-# Normalize
-
-X_train = X_train.astype('float32')
-X_val = X_val.astype('float32')
-X_train = X_train / 255 - 0.5
-X_val = X_val / 255 - 0.5
-Y_train = np_utils.to_categorical(y_train, 43)
-Y_val = np_utils.to_categorical(y_val, 43)
-
-# Preprocess
-
-X_test = test['features']
-y_test = test['labels']
-X_test = X_test.astype('float32')
-X_test /= 255
-X_test -= 0.5
-Y_test = np_utils.to_categorical(y_test, 43)
 
 # Model
 
 model = Sequential()
-model.add(Conv2D(32, 3, 3, input_shape=(32, 32, 3)))
+model.add(Conv2D(32, 3, 3, input_shape=(160, 320, 3)))
 model.add(MaxPooling2D((2,2)))
 model.add((Dropout(0.5)))
 model.add(Activation('relu'))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dense(43, activation='softmax'))
+model.add(Dense(1, activation='softmax'))
 model.summary()
 
 # Visualize
 
-plot(model, to_file='model.png')
+plot(model, to_file='model.png', show_shapes=True)
 
 # Train
 
-model.compile(loss='categorical_crossentropy',
+model.compile(loss='mse',
               optimizer='adam',
               metrics=['accuracy'])
-history = model.fit(X_train, Y_train,
-                    batch_size=128, nb_epoch=20,
-                    verbose=1, validation_data=(X_val, Y_val))
-    
+
+def generate_arrays_from_file(index_file, image_base):
+    while 1:
+        f = open(index_file)
+        next(f)
+        for line in f:
+            center, left, right, angle, throttle, brake, speed = line.split(",")
+            center = image_base + center.strip()
+            left = image_base + left.strip()
+            right = image_base + right.strip()
+            center = img.img_to_array(img.load_img(center))
+            left = img.img_to_array(img.load_img(left))
+            right = img.img_to_array(img.load_img(right))
+            angle = float(angle)
+            throttle = float(throttle)
+            brake = float(brake)
+            speed = float(speed)
+            batch_x = np.zeros((1,) + center.shape)
+            batch_y = np.zeros([1,1])
+            batch_x[0] = center
+            batch_y[0] = angle
+            yield (batch_x, batch_y)
+        f.close()
+                    
+history = model.fit_generator(generate_arrays_from_file("data/driving_log_head.csv", "data/"), 9, 5)
+
 # Test
 
 model.evaluate(X_test, Y_test)
-
 
 # Save
 
 model.save("model.h5")
 with open("model.json", "w") as f:
     f.write(model.to_json())
-
-
-
