d8759d688ea29d7227b0a1ee9ef2294f8eb7774b
David A. Ventimiglia <dventimi@gmail.com>
2017-01-07 08:02:35 -0800

    Simplifying the architecture for now

--- model.py
+++ model.py
@@ -45,15 +45,15 @@ image_shape = [160, 320, 3]
 input_shape = [x//2 for x in image_shape[:2]] + image_shape[2:]
 
 model = Sequential()
-model.add(Lambda(lambda x: x/127.5 - 1., input_shape=input_shape, output_shape=input_shape, trainable=False, name="Preprocess"))
-model.add(Conv2D(32, 3, 3, name="Conv2d"))
+# model.add(Lambda(lambda x: x/127.5 - 1., input_shape=input_shape, output_shape=input_shape, trainable=False, name="Preprocess"))
+model.add(Conv2D(32, 3, 3, name="Conv2d"), input_shape=input_shape)
 model.add(MaxPooling2D((2,2), name="MaxPool"))
 model.add((Dropout(0.5, name="Dropout")))
 model.add(Activation("relu", name="Activation"))
 model.add(Flatten(name="Flatten"))
 model.add(Dense(128, activation="relu", name="Fully-Connected"))
 model.add(Dense(1, activation="sigmoid", name="Readout"))
-model.add(Lambda(lambda x: 2.*x-1., trainable=False, name="Postprocess"))
+# model.add(Lambda(lambda x: 2.*x-1., trainable=False, name="Postprocess"))
 model.summary()
 
 # Visualize
