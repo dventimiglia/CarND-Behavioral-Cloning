ebb3fd8e7ac8a55cf2916bd1d63efd0e5a5fcd68
David A. Ventimiglia <dventimi@gmail.com>
2017-01-07 07:57:09 -0800

    Refactored the code a bit

--- model.py
+++ model.py
@@ -13,34 +13,9 @@ import numpy as np
 import pdb
 import pickle
 
-# Model
+# Utilities
 
-image_shape = [160, 320, 3]
-input_shape = [x//2 for x in image_shape[:2]] + image_shape[2:]
-
-model = Sequential()
-model.add(Lambda(lambda x: x/127.5 - 1., input_shape=input_shape, output_shape=input_shape, trainable=False, name="Preprocess"))
-model.add(Conv2D(32, 3, 3, name="Conv2d"))
-model.add(MaxPooling2D((2,2), name="MaxPool"))
-model.add((Dropout(0.5, name="Dropout")))
-model.add(Activation("relu", name="Activation"))
-model.add(Flatten(name="Flatten"))
-model.add(Dense(128, activation="relu", name="Fully-Connected"))
-model.add(Dense(1, activation="sigmoid", name="Readout"))
-model.add(Lambda(lambda x: 2.*x-1., trainable=False, name="Postprocess"))
-model.summary()
-
-# Visualize
-
-plot(model, to_file="model.png", show_shapes=True)
-
-# Train
-
-model.compile(loss="mse",
-              optimizer="adam",
-              metrics=["accuracy"])
-
-def generate_arrays_from_file(index_file, image_base):
+def input_generator(index_file, image_base):
     while 1:
         f = open(index_file)
         next(f)
@@ -64,14 +39,41 @@ def generate_arrays_from_file(index_file, image_base):
             yield (batch_x, batch_y)
         f.close()
 
-history = model.fit_generator(generate_arrays_from_file("data/driving_log_head.csv", "data/"), 9, 5)
+# Model
+
+image_shape = [160, 320, 3]
+input_shape = [x//2 for x in image_shape[:2]] + image_shape[2:]
+
+model = Sequential()
+model.add(Lambda(lambda x: x/127.5 - 1., input_shape=input_shape, output_shape=input_shape, trainable=False, name="Preprocess"))
+model.add(Conv2D(32, 3, 3, name="Conv2d"))
+model.add(MaxPooling2D((2,2), name="MaxPool"))
+model.add((Dropout(0.5, name="Dropout")))
+model.add(Activation("relu", name="Activation"))
+model.add(Flatten(name="Flatten"))
+model.add(Dense(128, activation="relu", name="Fully-Connected"))
+model.add(Dense(1, activation="sigmoid", name="Readout"))
+model.add(Lambda(lambda x: 2.*x-1., trainable=False, name="Postprocess"))
+model.summary()
+
+# Visualize
+
+plot(model, to_file="model.png", show_shapes=True)
+
+# Train
+
+model.compile(loss="mse", optimizer="adam", metrics=["accuracy"])
+
+datagen = input_generator("data/driving_log_head.csv", "data/")
+history = model.fit_generator(datagen, 9, 5, verbose=2)
 
 # Test
 
-model.evaluate(X_test, Y_test)
+# model.evaluate(X_test, Y_test)
 
 # Save
 
-model.save("model.h5")
-with open("model.json", "w") as f:
-    f.write(model.to_json())
+# model.save("model.h5")
+# with open("model.json", "w") as f:
+#     f.write(model.to_json())
+
