98280caa23e481df0e0b7e72d6d1ced80865ecfb
David A. Ventimiglia <dventimi@gmail.com>
2017-01-08 09:21:25 -0800

    Adapting the LeNet architecture

--- model.py
+++ model.py
@@ -16,28 +16,18 @@ import pickle
 
 # Utilities
 
-def input_generator(index_file, image_base):
+def input_generator(index_file, image_base, target_shape):
     while 1:
         f = open(index_file)
-        next(f)
         for line in f:
             center, left, right, angle, throttle, brake, speed = line.split(",")
-            center = image_base + center.strip()
-            left = image_base + left.strip()
-            right = image_base + right.strip()
-            center = cv2.imread(center)
-            center = cv2.resize(center, (input_shape[1], input_shape[0]), interpolation = cv2.INTER_AREA)
-            left = cv2.imread(left)
-            right = cv2.imread(right)
-            angle = float(angle)
-            throttle = float(throttle)
-            brake = float(brake)
-            speed = float(speed)
-            batch_x = np.zeros((1,) + center.shape)
-            batch_y = np.zeros([1,1])
-            batch_x[0] = center
-            batch_y[0] = angle
-            yield (batch_x, batch_y)
+            img = center
+            img = image_base + img.strip()
+            img = plt.imread(img)
+            img = cv2.resize(img, (target_shape[1], target_shape[0]), interpolation = cv2.INTER_AREA)
+            img = np.reshape(img, (1,)+img.shape)
+            angle = np.array([[float(angle)]])
+            yield (img, angle)
         f.close()
 
 # Model
@@ -45,20 +35,31 @@ def input_generator(index_file, image_base):
 image_shape = [160, 320, 3]
 input_shape = [x//2 for x in image_shape[:2]] + image_shape[2:]
 
-def CarND(in_shape):
+def LeNet(input_shape):
     model = Sequential()
-    model.add(Lambda(lambda x: x/127.5 - 1., input_shape=in_shape, output_shape=input_shape, trainable=False, name="Preprocess"))
-    model.add(Conv2D(32, 3, 3, name="Conv2d", input_shape=input_shape))
-    model.add(MaxPooling2D((2,2), name="MaxPool"))
-    model.add((Dropout(0.5, name="Dropout")))
-    model.add(Activation("relu", name="Activation"))
+    # Scale image values to [-1, 1]
+    model.add(Lambda(lambda x: x/127.5 - 1., input_shape=input_shape, output_shape=input_shape, trainable=False, name="Preprocess"))
+    model.add(Conv2D(6, 3, 3, name="Conv2D1", input_shape=input_shape))
+    model.add(Activation("relu", name="Activation1"))
+    model.add(MaxPooling2D((2,2), name="MaxPool1"))
+    model.add(Conv2D(10, 3, 3, name="Conv2D2"))
+    model.add(Activation("relu", name="Activation2"))
+    model.add(MaxPooling2D((2,2), name="MaxPool2"))
+    model.add(Conv2D(16, 3, 3, name="Conv2D3"))
+    model.add(Activation("relu", name="Activation3"))
+    model.add(MaxPooling2D((2,2), name="MaxPool3"))
     model.add(Flatten(name="Flatten"))
-    model.add(Dense(128, activation="relu", name="Fully-Connected"))
+    model.add(Dense(128, activation="relu", name="Fully-Connected1"))
+    model.add(Dense(64, activation="relu", name="Fully-Connected2"))
+    model.add(Dense(32, activation="relu", name="Fully-Connected3"))
+    model.add(Dense(16, activation="relu", name="Fully-Connected4"))
+    model.add(Dense(8, activation="relu", name="Fully-Connected5"))
+    model.add((Dropout(0.5, name="Dropout")))
     model.add(Dense(1, activation="sigmoid", name="Readout"))
     model.add(Lambda(lambda x: 2.*x-1., trainable=False, name="Postprocess"))
     return model
 
-model = CarND(input_shape)
+model = LeNet(input_shape)
 model.summary()
 
 # Visualize
@@ -68,16 +69,11 @@ plot(model, to_file="model.png", show_shapes=True)
 # Train
 
 model.compile(loss="mse", optimizer="adam", metrics=["accuracy"])
+datagen = input_generator("data/driving_log_three.csv", "data/", input_shape)
+history = model.fit_generator(datagen, samples_per_epoch=3, nb_epoch=20, verbose=2)
 
-datagen = input_generator("data/sample.csv", "data/")
-history = model.fit_generator(datagen, 10, 5, verbose=2)
-
-# Test
-
-# model.evaluate(X_test, Y_test)
-
-# Save
-
-# model.save("model.h5")
-# with open("model.json", "w") as f:
-#     f.write(model.to_json())
+for i in range(3):
+    a = datagen.__next__()
+    x = a[0]
+    y = a[1]
+    print(model.predict(x), y)
