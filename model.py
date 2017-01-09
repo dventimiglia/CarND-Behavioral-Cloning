6cdfdf0f03ee52adc521e2377ed70be5e322358d
David A. Ventimiglia <dventimi@gmail.com>
2017-01-08 19:31:35 -0800

    Minor changes

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 import cv2
 import keras.preprocessing.image as img
 import math
+import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
 import pdb
@@ -24,6 +25,7 @@ def input_generator(index_file, image_base, target_shape):
             img = center
             img = image_base + img.strip()
             img = plt.imread(img)
+            # img = mpimg.imread(img)
             img = cv2.resize(img, (target_shape[1], target_shape[0]), interpolation = cv2.INTER_AREA)
             img = np.reshape(img, (1,)+img.shape)
             angle = np.array([[float(angle)]])
@@ -39,26 +41,38 @@ def LeNet(input_shape):
     model = Sequential()
     # Scale image values to [-1, 1]
     model.add(Lambda(lambda x: x/127.5 - 1., input_shape=input_shape, output_shape=input_shape, trainable=False, name="Preprocess"))
-    model.add(Conv2D(6, 3, 3, name="Conv2D1", input_shape=input_shape))
-    model.add(Activation("relu", name="Activation1"))
+    model.add(Conv2D(6, 3, 3, name="Conv2D1", activation="relu", input_shape=input_shape))
     model.add(MaxPooling2D((2,2), name="MaxPool1"))
-    model.add(Conv2D(10, 3, 3, name="Conv2D2"))
-    model.add(Activation("relu", name="Activation2"))
+    model.add(Conv2D(10, 3, 3, activation="relu", name="Conv2D2"))
     model.add(MaxPooling2D((2,2), name="MaxPool2"))
-    model.add(Conv2D(16, 3, 3, name="Conv2D3"))
-    model.add(Activation("relu", name="Activation3"))
+    model.add(Conv2D(16, 3, 3, activation="relu", name="Conv2D3"))
     model.add(MaxPooling2D((2,2), name="MaxPool3"))
     model.add(Flatten(name="Flatten"))
-    model.add(Dense(128, activation="relu", name="Fully-Connected1"))
-    model.add(Dense(64, activation="relu", name="Fully-Connected2"))
-    model.add(Dense(32, activation="relu", name="Fully-Connected3"))
-    model.add(Dense(16, activation="relu", name="Fully-Connected4"))
-    model.add(Dense(8, activation="relu", name="Fully-Connected5"))
+    model.add(Dense(128, activation="relu", name="FC1"))
+    model.add(Dense(64, activation="relu", name="FC2"))
+    model.add(Dense(32, activation="relu", name="FC3"))
+    model.add(Dense(16, activation="relu", name="FC4"))
+    model.add(Dense(8, activation="relu", name="FC5"))
     model.add((Dropout(0.5, name="Dropout")))
     model.add(Dense(1, activation="sigmoid", name="Readout"))
     model.add(Lambda(lambda x: 2.*x-1., trainable=False, name="Postprocess"))
     return model
 
+def Nvidia(input_shape):
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5 - 1., input_shape=input_shape, output_shape=input_shape, trainable=False, name="Preprocess"))
+    model.add(Conv2D(24, 3, 3, name="Conv2D1", activation='relu', input_shape=input_shape))
+    model.add(Conv2D(36, 3, 3, name="Conv2D2", activation='relu'))
+    model.add(Conv2D(48, 3, 3, name="Conv2D2", activation='relu'))
+    model.add(Conv2D(64, 3, 3, name="Conv2D2", activation='relu'))
+    model.add(Conv2D(64, 3, 3, name="Conv2D2", activation='relu'))
+    model.add(Dense(1164, activation='relu', name="FC1"))
+    model.add(Dense(100, activation='relu', name="FC1"))
+    model.add(Dense(50, activation='relu', name="FC1"))
+    model.add(Dense(10, activation='relu', name="FC1"))
+    model.add(Dense(1, activation='sigmoid', name="FC1"))
+    return model
+                     
 model = LeNet(input_shape)
 model.summary()
 
@@ -69,15 +83,12 @@ plot(model, to_file="model.png", show_shapes=True)
 # Train
 
 model.compile(loss="mse", optimizer="adam", metrics=["accuracy"])
-datagen = input_generator("data/driving_log_overtrain.csv", "data/", input_shape)
-history = model.fit_generator(datagen, samples_per_epoch=3, nb_epoch=5, verbose=2)
 
-datagen = input_generator("data/driving_log_random_sample.csv", "data/", input_shape)
-history = model.fit_generator(datagen, samples_per_epoch=10, nb_epoch=5, verbose=2)
+# datagen = input_generator("data/driving_log_overtrain.csv", "data/", input_shape)
+# history = model.fit_generator(datagen, samples_per_epoch=3, nb_epoch=5, verbose=2)
 
-datagen = input_generator("data/driving_log_all.csv", "data/", input_shape)
-history = model.fit_generator(datagen, samples_per_epoch=100, nb_epoch=5, verbose=2)
+# datagen = input_generator("data/driving_log_random_sample.csv", "data/", input_shape)
+# history = model.fit_generator(datagen, samples_per_epoch=10, nb_epoch=5, verbose=2)
 
 datagen = input_generator("data/driving_log_train.csv", "data/", input_shape)
-history = model.fit_generator(datagen, samples_per_epoch=100, nb_epoch=5, verbose=2)
-
+history = model.fit_generator(datagen, samples_per_epoch=1000, nb_epoch=7, verbose=2)
